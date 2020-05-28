f442fb33246442e1f6b940d7e4e49a5f259937c0
Edmund Johnson <ekjjr777@gmail.com>
2020-05-28 11:50:59 -0500

    OrangeTree Spec

---
+++ spec/OrangeTree.spec.js
@@ -0,0 +1,34 @@
+const assert = require('assert');
+const OrangeTree = require('../OrangeTree.js');
+
+describe("OrangeTree class", function() {
+
+  // 1) Create a test resource
+  // 2) Create conditions for test to pass or fail and verify
+  it("should set the age and height to zero upon creation", function() {
+
+  });
+
+  it("should be mature after age 6", function() {
+
+
+  });
+
+  it("should be mature at age of 6", function() {
+
+  });
+
+  it("should age one year after passGrowingSeason", function() {
+
+  });
+
+  it("should grow 2.5 feet in height after passGrowingSeason", function() {
+
+  });
+
+  it("should not go past maximum height of 25 after passGrowingSeason", function() {
+
+  });
+
+
+});
